0d8b06caf8009742edbe33bded48e016ffc5e6c4
deepaksharma955 <deepaksharma955@gmail.com>
2026-08-24 14:02:29 +0530

    fix: persist Firebase Auth state across app restarts using AsyncStorage
    - Replaced getAuth() with initializeAuth() + getReactNativePersistence(AsyncStorage)
    - Auth state now persists between sessions, no more repeated logins

--- firebase.js
+++ firebase.js
@@ -1,40 +1,23 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
-
-// TODO: Replace with your actual Firebase project configuration
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "messenger-a5efb",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "1:798969223897:android:913b48e70c767e83dbfcc8"
-// };
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyAZ0jNd_TYYsSEWSK8MWSG553j268FiLrg",
-
   authDomain: "messenger-a5efb.firebaseapp.com",
-
   databaseURL: "https://messenger-a5efb-default-rtdb.firebaseio.com",
-
   projectId: "messenger-a5efb",
-
   storageBucket: "messenger-a5efb.appspot.com",
-
   messagingSenderId: "798969223897",
-
   appId: "1:798969223897:web:99e098c1ea7a15a5dbfcc8",
-
   measurementId: "G-Q0SKL16DSQ"
-
 };
 
-
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const database = getFirestore(app);
 export const realtimeDb = getDatabase(app);
